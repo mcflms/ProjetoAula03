8124305386fefd82da03c43f0ea808e5fa2e849f
Marcio das Chagas <mcflms@gmail.com>
2024-03-20 20:52:49 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoAula03/Entities/Empresa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAula03.Entities
+
+    ///<summary>
+    ///Classe de Entidade
+    ///</summary>
+{
+    public class Empresa
+    {
+        public Guid Id { get; set; }
+        public string NomeFantasia { get; set; }
+        public string RazaoSocial { get; set; }
+        public string Cnpj { get; set; }
+
+        //Associação de um para muitos, ou seja, de uma Empresa para muitos Funcionarios. 
+        #region Associações
+        public List<Funcionario> Funcionarios { get; set; }
+        #endregion
+    }
+
+
+}
---
+++ ProjetoAula03/Entities/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAula03.Entities
+{
+    /// <summary>
+    /// Classse de Entidade
+    /// </summary>
+    public class Funcionario
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Matricula { get; set; }
+        public int Cpf { get; set; }
+        public DateTime DataAdmissao { get; set; }
+        public Guid EmpresaId { get; set; }
+
+        //Associação de muitos para um, ou seja de Funcionarios a Empresa. 
+        #region Assiciações
+        public Empresa Empresa { get; set; }
+
+        #endregion
+    }
+}
